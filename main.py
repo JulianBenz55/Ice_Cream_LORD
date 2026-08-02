d582c17e1e22dc2ad4d92637178484a263cd62d9
JulianBenz55 <julianbenz366@gmail.com>
2026-08-02 13:41:47 +0200

    Add missing Query import to main.py to fix runtime NameError

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, Query
 from fastapi.responses import StreamingResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
